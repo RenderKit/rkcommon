7b0f276cf3e22eddaf7365512bc304dea130cf60
Jefferson Amstutz <jefferson.d.amstutz@intel.com>
2019-08-15 16:36:33 -0500

    use OSPCOMMON_TBB_ROOT instead of TBB_ROOT to avoid CMake warnings

--- cmake/FindTBB.cmake
+++ cmake/FindTBB.cmake
@@ -16,16 +16,19 @@
 
 set(TBB_VERSION_REQUIRED "3.0")
 
-if (NOT TBB_ROOT)
-  set(TBB_ROOT $ENV{TBB_ROOT})
+if (NOT OSPCOMMON_TBB_ROOT)
+  set(OSPCOMMON_TBB_ROOT $ENV{OSPCOMMON_TBB_ROOT})
 endif()
-if (NOT TBB_ROOT)
-  set(TBB_ROOT $ENV{TBBROOT})
+if (NOT OSPCOMMON_TBB_ROOT)
+  set(OSPCOMMON_TBB_ROOT $ENV{TBB_ROOT})
+endif()
+if (NOT OSPCOMMON_TBB_ROOT)
+  set(OSPCOMMON_TBB_ROOT $ENV{TBBROOT})
 endif()
 
-## Detect changed TBB_ROOT ##
+## Detect changed OSPCOMMON_TBB_ROOT ##
 
-if (NOT TBB_ROOT STREQUAL TBB_ROOT_LAST)
+if (NOT OSPCOMMON_TBB_ROOT STREQUAL OSPCOMMON_TBB_ROOT_LAST)
   unset(TBB_INCLUDE_DIR CACHE)
   unset(TBB_LIBRARY CACHE)
   unset(TBB_LIBRARY_DEBUG CACHE)
@@ -43,9 +46,9 @@ if (WIN32)
   if (NOT PROGRAMFILES32)
     set(PROGRAMFILES32 "C:/Program Files (x86)")
   endif()
-  find_path(TBB_ROOT include/tbb/task_scheduler_init.h
+  find_path(OSPCOMMON_TBB_ROOT include/tbb/task_scheduler_init.h
     DOC "Root of TBB installation"
-    HINTS ${TBB_ROOT}
+    HINTS ${OSPCOMMON_TBB_ROOT}
     PATHS
       ${PROJECT_SOURCE_DIR}/tbb
       ${PROJECT_SOURCE_DIR}/../tbb
@@ -60,9 +63,9 @@ if (WIN32)
     set(TBB_ARCH ia32)
   endif()
 
-  set(TBB_LIBDIR ${TBB_ROOT}/lib)
+  set(TBB_LIBDIR ${OSPCOMMON_TBB_ROOT}/lib)
 
-  find_path(TBB_INCLUDE_DIR tbb/task_scheduler_init.h PATHS ${TBB_ROOT}/include NO_DEFAULT_PATH)
+  find_path(TBB_INCLUDE_DIR tbb/task_scheduler_init.h PATHS ${OSPCOMMON_TBB_ROOT}/include NO_DEFAULT_PATH)
   set(TBB_LIB_HINTS
     PATHS
     ${TBB_LIBDIR}/${TBB_ARCH}/vc14
@@ -76,9 +79,9 @@ if (WIN32)
 
 else ()
 
-  find_path(TBB_ROOT include/tbb/task_scheduler_init.h
+  find_path(OSPCOMMON_TBB_ROOT include/tbb/task_scheduler_init.h
     DOC "Root of TBB installation"
-    HINTS ${TBB_ROOT}
+    HINTS ${OSPCOMMON_TBB_ROOT}
     PATHS
       ${PROJECT_SOURCE_DIR}/tbb
       /opt/intel/composerxe/tbb
@@ -87,14 +90,14 @@ else ()
   )
 
   if (APPLE)
-    find_path(TBB_INCLUDE_DIR tbb/task_scheduler_init.h PATHS ${TBB_ROOT}/include NO_DEFAULT_PATH)
-    find_library(TBB_LIBRARY tbb PATHS ${TBB_ROOT}/lib NO_DEFAULT_PATH)
-    find_library(TBB_LIBRARY_DEBUG tbb_debug PATHS ${TBB_ROOT}/lib NO_DEFAULT_PATH)
-    find_library(TBB_LIBRARY_MALLOC tbbmalloc PATHS ${TBB_ROOT}/lib NO_DEFAULT_PATH)
-    find_library(TBB_LIBRARY_MALLOC_DEBUG tbbmalloc_debug PATHS ${TBB_ROOT}/lib NO_DEFAULT_PATH)
+    find_path(TBB_INCLUDE_DIR tbb/task_scheduler_init.h PATHS ${OSPCOMMON_TBB_ROOT}/include NO_DEFAULT_PATH)
+    find_library(TBB_LIBRARY tbb PATHS ${OSPCOMMON_TBB_ROOT}/lib NO_DEFAULT_PATH)
+    find_library(TBB_LIBRARY_DEBUG tbb_debug PATHS ${OSPCOMMON_TBB_ROOT}/lib NO_DEFAULT_PATH)
+    find_library(TBB_LIBRARY_MALLOC tbbmalloc PATHS ${OSPCOMMON_TBB_ROOT}/lib NO_DEFAULT_PATH)
+    find_library(TBB_LIBRARY_MALLOC_DEBUG tbbmalloc_debug PATHS ${OSPCOMMON_TBB_ROOT}/lib NO_DEFAULT_PATH)
   else()
-    find_path(TBB_INCLUDE_DIR tbb/task_scheduler_init.h PATHS ${TBB_ROOT}/include NO_DEFAULT_PATH)
-    set(TBB_HINTS HINTS ${TBB_ROOT}/lib/intel64/gcc4.7 ${TBB_ROOT}/lib/intel64/gcc4.4 ${TBB_ROOT}/lib ${TBB_ROOT}/lib64 PATHS /usr/libx86_64-linux-gnu/)
+    find_path(TBB_INCLUDE_DIR tbb/task_scheduler_init.h PATHS ${OSPCOMMON_TBB_ROOT}/include NO_DEFAULT_PATH)
+    set(TBB_HINTS HINTS ${OSPCOMMON_TBB_ROOT}/lib/intel64/gcc4.7 ${OSPCOMMON_TBB_ROOT}/lib/intel64/gcc4.4 ${OSPCOMMON_TBB_ROOT}/lib ${OSPCOMMON_TBB_ROOT}/lib64 PATHS /usr/libx86_64-linux-gnu/)
     find_library(TBB_LIBRARY libtbb.so.2 ${TBB_HINTS})
     find_library(TBB_LIBRARY_DEBUG libtbb_debug.so.2 ${TBB_HINTS})
     find_library(TBB_LIBRARY_MALLOC libtbbmalloc.so.2 ${TBB_HINTS})
@@ -102,11 +105,11 @@ else ()
   endif()
 endif()
 
-set(TBB_ROOT_LAST ${TBB_ROOT} CACHE INTERNAL "Last value of TBB_ROOT to detect changes")
+set(OSPCOMMON_TBB_ROOT_LAST ${OSPCOMMON_TBB_ROOT} CACHE INTERNAL "Last value of OSPCOMMON_TBB_ROOT to detect changes")
 
 set(TBB_ERROR_MESSAGE
   "Threading Building Blocks (TBB) with minimum version ${TBB_VERSION_REQUIRED} not found.
-OSPRay uses TBB as default tasking system. Please make sure you have the TBB headers installed as well (the package is typically named 'libtbb-dev' or 'tbb-devel') and/or hint the location of TBB in TBB_ROOT.
+OSPRay uses TBB as default tasking system. Please make sure you have the TBB headers installed as well (the package is typically named 'libtbb-dev' or 'tbb-devel') and/or hint the location of TBB in OSPCOMMON_TBB_ROOT.
 Alternatively, you can try to use OpenMP as tasking system by setting OSPCOMMON_TASKING_SYSTEM=OpenMP")
 
 include(FindPackageHandleStandardArgs)
