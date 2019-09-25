e4befe7c569ace5469507741d5f85ec7e3a55c9e
Will Usher <will@sci.utah.edu>
2019-09-25 16:06:39 -0600

    fix pragma

--- ospcommon/networking/Fabric.cpp
+++ ospcommon/networking/Fabric.cpp
@@ -14,8 +14,6 @@
 // limitations under the License.                                           //
 // ======================================================================== //
 
-#pragma once
-
 #include <cstdlib>
 #include <memory>
 #include "../common.h"
