eafbc29ca48c75540490c076e4394ed9a7120c16
Johannes Günther <johannes.guenther@intel.com>
2022-05-05 11:20:21 +0200

    Switch to single-year Copyright format

--- rkcommon/utility/ArgumentList.h
+++ rkcommon/utility/ArgumentList.h
@@ -1,4 +1,4 @@
-// Copyright 2009-2020 Intel Corporation
+// Copyright 2009 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
 /*! \file ArgumentList.h Defines an interface for storing - and consuming -
