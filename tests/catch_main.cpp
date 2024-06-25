259f2cccaf71b69d56cba59707b69fc32588a387
Miroslaw Pawlowski <miroslaw.pawlowski@intel.com>
2024-06-25 18:04:35 +0200

    Add initTaskingSystem() call to test suite main()

--- tests/catch_main.cpp
+++ tests/catch_main.cpp
@@ -1,5 +1,11 @@
 // Copyright 2009 Intel Corporation
 // SPDX-License-Identifier: Apache-2.0
 
-#define CATCH_CONFIG_MAIN
+#define CATCH_CONFIG_RUNNER
 #include "catch.hpp"
+#include "rkcommon/tasking/tasking_system_init.h"
+
+int main(int argc, char* argv[]) {
+  rkcommon::tasking::initTaskingSystem();
+  return Catch::Session().run(argc, argv);
+}
